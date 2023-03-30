886576eab5dc2a2feef52061731e9c8486a57747
Pedro Kirstein <brksphkp@gmail.com>
2023-03-30 20:07:09 -0300

    integration with firebase

--- src/configuration/ServiceApi.js
+++ src/configuration/ServiceApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:5001/api",
+  baseURL: "https://lawyersysbackend-production.up.railway.app/",
 });
 
 export default api;
--- src/pages/register/RegisterService.js
+++ src/pages/register/RegisterService.js
@@ -1,7 +1,5 @@
-import Api from '../../configuration/ServiceApi';
 import axios from "axios";
 
-// const getAddres = (cep) => Api.get(`viacep.com.br/ws/${cep}/json/`);
 const getAddres = (cep) => axios.get(`https://viacep.com.br/ws/${cep}/json/`);
 
 export default { getAddres }
